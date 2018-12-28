aa0a983f2e4116f40861736a8cb312b37abf414d
Pascal <muster.pascal@t-online.de>
2018-12-28 21:29:34 +0100

    Changed for class MyRuuvi
    Signed-off-by: Pascal <muster.pascal@t-online.de>

--- examples/print_to_screen.2.py
+++ examples/print_to_screen.2.py
@@ -2,11 +2,16 @@
 Printing Data to Shell Window
 Press Ctrl+C to escape running sequence
 '''
-
+from time import sleep
 from ruuvitag_sensor.testing import MyRuuvi
 
-mac = 'E8:C7:D7:F2:4B:47'
-sleepTime = 0
-
-ruuvi_sensor = MyRuuvi(mac)
-ruuvi_sensor.print_to_shell(sleepTime)
+sensor = MyRuuvi('E8:C7:D7:F2:4B:47')
+while True:
+    try:
+        sensor.update()
+        sensor.print_to_shell()
+        sleep(1)
+    except:
+        # On KeyError or KeyInterrupt exit loop
+        print('\nExit')
+        break
